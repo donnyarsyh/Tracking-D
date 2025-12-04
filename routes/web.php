a4e9d116412c06026a419020cb15869a7083cd19
itsdrsw <marswan27@gmail.com>
2025-12-04 11:46:11 +0700

    feat(routes): define auth, profile, and protected page routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,39 @@
 <?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('landingPage');
-});
+})->name('landing');
+Route::get('/login', function () {
+    return redirect('/'); // arahkan ke halaman home (karena login pakai modal)
+})->name('login');
+
+
+// AUTH Routes
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [LoginController::class, 'register'])->name('register.process');
+Route::post('/logout', [LoginController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout.process');
+
 
 Route::get('/diabetes', function () {
     return view('diabetes');
-})->name('diabetes.page');
+})->middleware('auth')->name('diabetes.page');
 
 Route::get('/lifestyle', function () {
     return view('lifestyle');
-})->name('lifestyle.page');
+})->middleware('auth')->name('lifestyle.page');
+
+// PROFILE Routes
+Route::post('/profile/update', [ProfileController::class, 'update'])
+    ->middleware('auth')
+    ->name('profile.update');
+
 
 Route::get('/profile', function () {
     return view('profile');
-})->name('profile.page');
+})->middleware('auth')->name('profile.page');
